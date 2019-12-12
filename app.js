6807720855c4ea367a26891d28fa0ed1ffb84da5
Sean Cassidy <seca.design@icloud.com>
2019-12-12 14:54:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-
-    window.location.href = 'https://55pq0g.axshare.com/#id=h4uqwn&p=check_in_complete&g=1';
+    window.location.href = 'https://55pq0g.axshare.com/#id=h4uqwn&p=check_in_complete';
 
     // track.stop();
 };
